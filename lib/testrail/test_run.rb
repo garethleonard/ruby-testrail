1363036a2f1b994f0e34a59c7a8225325d758c29
Javier Durante <javier.durante@findly.com>
2016-04-27 12:03:02 +1200

    extract classes to separated files

---
+++ lib/testrail/test_run.rb
@@ -0,0 +1,32 @@
+module TestRail
+
+  class TestRun
+
+    def initialize(suite:, id:)
+      @suite = suite
+      @id = id
+      @results = []
+    end
+
+    def add_test_result(section_name:, test_name:, success:, comment: nil)
+      @results << @suite
+        .get_or_create_section(section_name)
+        .get_or_create_test_case(test_name)
+        .create_result(success: success, comment: comment)
+    end
+
+    def submit_results
+      @suite.submit_test_results(run_id: @id, results: @results)
+    end
+
+    def close
+      @suite.close_test_run(@id)
+    end
+
+    def failure_count
+      @results.count { |r| !r.success? }
+    end
+
+  end
+
+end
