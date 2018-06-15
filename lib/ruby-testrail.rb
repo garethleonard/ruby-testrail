bb903763f780032e6082d643a51b3b88f0a7b9b8
Gareth Leonard <gareth.leonard@csaa.com>
2018-06-15 14:23:48 -0700

    Add ruby-testrail.rb

---
+++ lib/ruby-testrail.rb
@@ -0,0 +1,12 @@
+require 'testrail/adaptor'
+require 'testrail/api_client'
+require 'testrail/cucumber_adaptor'
+require 'testrail/rspec_adaptor'
+require 'testrail/test_case'
+require 'testrail/test_result'
+require 'testrail/test_run'
+require 'testrail/test_section'
+require 'testrail/test_suite'
+require 'testrail/testrail_client'
+
+module TestRail; end
